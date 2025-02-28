68f415a2540fa39b628a39811fd264703e62ac19
KishanPatel99 <kishansanjaykumar.patel5514@coyote.csusb.edu>
2025-02-28 13:05:07 -0800

    Fix the files!

--- docker-setup.sh
+++ docker-setup.sh
@@ -2,6 +2,7 @@
 
 # Define container name
 CONTAINER_NAME="team3s25-app"
+VOLUME_NAME="scrapy_output"
 PORT_NUM=2503
 J_PORTNUM=2513
 
@@ -9,9 +10,32 @@ J_PORTNUM=2513
 echo "Enter your Groq-API key:"
 read GROQ_API_KEY
 
+# Check if the volume exists
+if [ $(docker volume ls -q -f name=$VOLUME_NAME) ]; then
+    echo "Volume '$VOLUME_NAME' already exists."
+else
+    echo "Creating volume '$VOLUME_NAME'..."
+    docker volume create $VOLUME_NAME
+fi
+
 # Build the Docker image
 echo "Building Docker image..."
 docker build -t $CONTAINER_NAME .
+
+# Check if a container with the same name is already running
+if [ $(docker ps -q -f name=$CONTAINER_NAME) ]; then
+    echo "Stopping existing container..."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+fi
+
+# Run the Docker container
+echo "Running Docker container..."
+docker run -d -p 2503:2503 -p 2513:2513 --name $CONTAINER_NAME -e GROQ_API_KEY="$GROQ_API_KEY" -v $VOLUME_NAME:/data $CONTAINER_NAME
+
+echo "Docker container is running. Access your services at:"
+echo "- Streamlit: http://localhost:2503"
+echo "- Jupyter Notebook: http://localhost:2513"
 
 # Running Docker Image
 docker run -d -p $PORT_NUM:$PORT_NUM -p $J_PORTNUM:$J_PORTNUM --name $CONTAINER_NAME -e GROQ_API_KEY="$GROQ_API_KEY" $CONTAINER_NAME
