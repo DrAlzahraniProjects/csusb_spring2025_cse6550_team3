055e9b72d6d61f37fbfedeea9de689f899a6b8a8
Nikita4893 <nikita4893@coyote.csusb.edu>
2025-04-12 12:03:09 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+# Start Apache in the background
+apache2ctl start
+
+# Start your Scrapy spider in the background
+python3 -u go-paper-spider.py &
+
+# Start Streamlit in the background
+streamlit run app.py --server.port=2503 --server.baseUrlPath=/team3s25 &
+
+# This line keeps the container alive and makes docker ps show "python app.py"
+python app.py
 
