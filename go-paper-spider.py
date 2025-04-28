41a039339afffdb90f5043db50bedc2ad39bf3e8
Raymond474 <mraymond474@gmail.com>
2025-04-28 09:25:55 -0700

    Fixed Spider not starting and changed to json

--- go-paper-spider.py
+++ go-paper-spider.py
@@ -3,7 +3,7 @@ from scrapy.utils.project import get_project_settings
 from mycrawler.spiders.paperswithcode_spider import PaperSpider
 
 # Path (mapped to Docker volume)
-papers_output_file_path = "/data/papers_output.csv"
+papers_output_file_path = "/data/papers_output.json"
 
 def run_paper_spider():
     """Run the PapersSpider and save the output."""
@@ -11,14 +11,21 @@ def run_paper_spider():
     settings = get_project_settings()
     settings.update({
         'FEED_URI': papers_output_file_path,
-        'FEED_FORMAT': 'csv',
-        'FEED_EXPORT_INDENT': 4,
+        'FEED_FORMAT': 'json', 
+        'FEED_EXPORT_INDENT': 4, 
         'FEED_EXPORT_ENCODING': 'utf-8',
         'FEED_OVERWRITE': False,
         'FEED_STORE_EMPTY': False,
-        'FEED_EXPORT_FIELDS': ["text"],  # Ensure consistent column order
+        'FEED_EXPORT_FIELDS': [
+            "url", "title", "abstract", "date", "authors",
+            "artefact-information", "description", "benchmark_dataset_links",
+            "dataset_names", "best_model_names", "dataset_links",
+            "paper_list_titles", "paper_list_title_links",
+            "paper_list_authors", "paper_list_author_links",
+            "paper_list_dates", "paper_list_abstracts"
+        ],
         'FEED_EXPORTERS': {
-        'csv': 'mycrawler.exporters.HeadlessCsvItemExporter',
+            'json': 'scrapy.exporters.JsonItemExporter',  # Use Scrapy's built-in JSON exporter
         },
     })
 
